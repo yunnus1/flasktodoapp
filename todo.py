933fc690c27f4636bb733050f752c5a607561497
yunnus1 <cyunusnadi@gmail.com>
2020-06-01 21:31:39 +0300

    Flask First Commit

---
+++ todo.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,redirect,url_for,request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/yunnus1/Desktop/TodoAP/todo.db'
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    todos = Todo.query.all()
+    return render_template("index.html", todos = todos)
+
+@app.route("/add", methods = ["POST"])
+def addTodo():
+
+    title = request.form.get("title")
+    newTodo = Todo(title = title , complete = False)
+    db.session.add(newTodo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+class Todo(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     title = db.Column(db.String(80))
+     complete = db.Column(db.Boolean)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True) 
